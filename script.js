79f84c92084b3df44cf9122e9311747869957e18
Daniel Chin <danielchin@Daniels-MacBook-Pro.local>
2025-02-07 16:11:58 -0700

    practice syntax

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('hello world')
+// make a line
+// make a square
+// make a pyramid
+
+
+let lineMaker = function(num) {
+  let line = ''
+  for (let i = 0; i < num; i += 1) {
+    line += 'x'
+  }
+  return line;
+}
+
+console.log(lineMaker(3))
+
+function printSquare(num) {
+  for (let i = 0; i < num; i++) {
+    let row = "1";
+    for (let j = 0; j < num; j++) {
+      row += "* ";
+    }
+    console.log(row);
+  }
+}
+
+console.log(printSquare(5))
